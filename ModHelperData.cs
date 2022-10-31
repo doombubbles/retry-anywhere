9b0d40ddb584d6341bc799a5daafae2570b857b8
doombubbles <obsidiantoolzg@gmail.com>
2022-10-30 20:48:36 -0700

    Update icon

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,10 @@ public static class ModHelperData
     public const string Name = "Retry Anywhere";
 
     public const string Description = "Makes the Retry Round button show up and be usable in all modes.\n" +
-                                      "Only works for Bosses when playing games using the Boss Rounds mod.";
+                                      "Only works for Bosses when playing games using the Boss Rounds mod.\n\n" +
+                                      "(This is basically just a newer take on my other mod, Auto Escape)";
+
+    public const bool SquareIcon = true;
 
     public const string RepoOwner = "doombubbles";
     public const string RepoName = "retry-anywhere";
